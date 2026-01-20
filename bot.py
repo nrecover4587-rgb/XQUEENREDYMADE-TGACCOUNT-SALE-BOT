3022ab7d5c32f561bbc9ee91a1de12c7e0572653
nrecover4587-rgb <nrecover4587@gmail.com>
2026-01-20 19:19:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,8 +21,8 @@ from pyrogram.errors import (
 # -----------------------
 # CONFIG
 # -----------------------
-BOT_TOKEN = os.getenv('BOT_TOKEN', 'pest')
-ADMIN_ID = int(os.getenv('ADMIN_ID', '7582601826'))
+BOT_TOKEN = os.getenv('BOT_TOKEN', '8429714477:AAFcY8-MnKrg-EaXHg_b8AoPe20OwqTU36c')
+ADMIN_ID = int(os.getenv('ADMIN_ID', '8364016757'))
 MONGO_URL = os.getenv('MONGO_URL', 'mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority')
 API_ID = int(os.getenv('API_ID', '30038466'))
 API_HASH = os.getenv('API_HASH', '5a492a0dfb22b1a0b7caacbf90cbf96e')
