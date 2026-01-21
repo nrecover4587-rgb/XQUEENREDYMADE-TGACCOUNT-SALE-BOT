2e21cca5eb7237ae67f92cd9579c9d39076e76f7
nrecover4587-rgb <nrecover4587@gmail.com>
2026-01-21 11:17:44 +0530

    Update and rename bot (19).py to bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ from pyrogram.errors import (
 # -----------------------
 # CONFIG
 # -----------------------
-BOT_TOKEN = os.getenv('BOT_TOKEN', '8543596764:AAH68BMGTpPDTgaYHinhIq-AFrLzQPtCGBg')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '8222658331:AAFBsTXTJsgGA-ZyIgh3jWhfSP4HwlLw1vo')
 ADMIN_ID = int(os.getenv('ADMIN_ID', '7582601826'))
 MONGO_URL = os.getenv('MONGO_URL', 'mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority')
 API_ID = int(os.getenv('API_ID', '30038466'))
